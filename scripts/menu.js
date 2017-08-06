897b11f0245c886700eab9b5fc43e38c522462e7
Zanlue <30582227+Zanlue@users.noreply.github.com>
2017-08-05 20:17:09 -0700

    Added Menu for Navigation

---
+++ scripts/menu.js
@@ -0,0 +1,15 @@
+function toggleMenu(x) {
+	var buttonOpen = document.getElementsByClassName("nav-button");
+	var buttonClose = document.getElementsByClassName("nav-close");
+	var button = document.getElementsByClassName("button-wrap");
+	var menu = document.getElementsByClassName("menu-container");
+	
+	if (x == 'open') {
+		$(button[0]).hide("slide", {direction: "down"}, 200, function(){ $(button[1]).show("slide", {direction: "down"}, 200)});
+		$(menu).show("slide", {direction: "down"}, 800).delay(500);
+	}
+	else if (x == 'close') {
+		$(button[1]).hide("slide", {direction: "down"}, 200, function(){ $(button[0]).show("slide", {direction: "down"}, 200)});
+		$(menu).hide("slide", {direction: "up"}, 800).delay(500);
+	};
+}
