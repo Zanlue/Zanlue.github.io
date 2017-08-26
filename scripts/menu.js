459b3c638a7db94f627ed56493008e101a680696
Zanlue <30582227+Zanlue@users.noreply.github.com>
2017-08-26 00:10:30 -0700

    Cleaned Up
    - Moved stuff around
    - Changed some fonts
    - Adjusted positioning of some elements

--- scripts/menu.js
+++ scripts/menu.js
@@ -5,11 +5,11 @@ function toggleMenu(x) {
 	var menu = document.getElementsByClassName("menu-container");
 	
 	if (x == 'open') {
-		$(button[0]).hide("slide", {direction: "down"}, 200, function(){ $(button[1]).show("slide", {direction: "down"}, 200)});
-		$(menu).show("slide", {direction: "down"}, 800).delay(500);
+		$(button[0]).hide("slide", {direction: "down"}, 200);
+		$(menu).show("slide", {direction: "down"}, 800, function(){ $(button[1]).show("slide", {direction: "down"}, 200)}).delay(500);
 	}
 	else if (x == 'close') {
-		$(button[1]).hide("slide", {direction: "down"}, 200, function(){ $(button[0]).show("slide", {direction: "down"}, 200)});
-		$(menu).hide("slide", {direction: "up"}, 800).delay(500);
+		$(button[1]).hide("slide", {direction: "down"}, 200);
+		$(menu).hide("slide", {direction: "up"}, 800, function(){ $(button[0]).show("slide", {direction: "down"}, 200)}).delay(500);
 	};
 }
