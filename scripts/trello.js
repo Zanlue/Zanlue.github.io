2ceaa2cc9a762b26854ce59464287cbdc3ce1d65
Zanlue <30582227+Zanlue@users.noreply.github.com>
2017-10-07 23:43:45 -0700

    Trello Webhook: Commit #15

--- scripts/trello.js
+++ scripts/trello.js
@@ -29,9 +29,18 @@ window.Trello.authorize({
   error: authenticationFailure
 });
 
-window.Trello.post("https://api.trello.com/1/tokens/7b204829f2a896be8064ed02273e01f32203cf755326c1ebf41974763d812ce4/webhooks/?key=d03c9cf29a52ada649aae8f04b793652", {
+/*window.Trello.post("https://api.trello.com/1/tokens/7b204829f2a896be8064ed02273e01f32203cf755326c1ebf41974763d812ce4/webhooks/?key=d03c9cf29a52ada649aae8f04b793652", {
   description: "My first webhook",
   callbackURL: "https://discordapp.com/api/webhooks/366444051274399745/_hlIRmZTkTdvHAdPj7IrMc3D4dQYKSfgno1Z9xO6IqyynHQoDKmIbaeeyYicip5py5sQ",
   idModel: "58a3c6efd74935bca133a8db",
   active: true,
-} console.log('Successful post'), console.log('Failed post'));
+});*/
+
+$.ajax({
+	type: 'POST',
+	url: 'https://api.trello.com/1/tokens/7b204829f2a896be8064ed02273e01f32203cf755326c1ebf41974763d812ce4/webhooks/?key=d03c9cf29a52ada649aae8f04b793652',
+	data: {
+		idModel: "58a3c6efd74935bca133a8db",
+		callbackURL: "https://discordapp.com/api/webhooks/366444051274399745/_hlIRmZTkTdvHAdPj7IrMc3D4dQYKSfgno1Z9xO6IqyynHQoDKmIbaeeyYicip5py5sQ"
+	},
+});
