1bbf2eeb87c7baef01249544372cd37059e9255d
Zanlue <30582227+Zanlue@users.noreply.github.com>
2017-10-07 21:12:42 -0700

    Trello Webhook: Commit #7

--- scripts/trello.js
+++ scripts/trello.js
@@ -1,11 +1,11 @@
-var WHITE_ICON = '../assets/icon-white.svg';
+var WHITE_ICON = 'https://zanlue.github.io/assets/icon-white.svg';
 
 window.TrelloPowerUp.initialize({
   'board-buttons': function(t, options){
     return [{
       icon: WHITE_ICON,
       text: 'Webhook',
-      callback: function (t, opts) { ... }
+      callback: function (t, opts) { console.log('Someone clicked the button'); }
     }];
   }
 });
