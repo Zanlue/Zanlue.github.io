057d099e317e30740d54dec536a30edc750c45b7
Zanlue <30582227+Zanlue@users.noreply.github.com>
2017-10-07 22:19:38 -0700

    Trello Webhook: Commit #10

--- scripts/trello.js
+++ scripts/trello.js
@@ -10,7 +10,26 @@ window.TrelloPowerUp.initialize({
   }
 });
 
-window.TrelloPowerUp.post("https://api.trello.com/1/tokens/7b204829f2a896be8064ed02273e01f32203cf755326c1ebf41974763d812ce4/webhooks/?key=d03c9cf29a52ada649aae8f04b793652", {
+var authenticationSuccess = function() {
+  console.log('Successful authentication');
+};
+
+var authenticationFailure = function() {
+  console.log('Failed authentication');
+};
+
+window.Trello.authorize({
+  type: 'popup',
+  name: 'Getting Started Application',
+  scope: {
+    read: 'true',
+    write: 'true' },
+  expiration: 'never',
+  success: authenticationSuccess,
+  error: authenticationFailure
+});
+
+$.post("https://api.trello.com/1/tokens/7b204829f2a896be8064ed02273e01f32203cf755326c1ebf41974763d812ce4/webhooks/?key=d03c9cf29a52ada649aae8f04b793652", {
   description: "My first webhook",
   callbackURL: "https://discordapp.com/api/webhooks/366444051274399745/_hlIRmZTkTdvHAdPj7IrMc3D4dQYKSfgno1Z9xO6IqyynHQoDKmIbaeeyYicip5py5sQ",
   idModel: "58a3c6efd74935bca133a8db",
