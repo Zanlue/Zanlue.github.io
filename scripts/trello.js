adf9c71c7f6219f1b04a930bae17fcfe61ab46a1
Zanlue <30582227+Zanlue@users.noreply.github.com>
2017-10-08 00:37:16 -0700

    Trello Webhook: Commit #17

--- scripts/trello.js
+++ scripts/trello.js
@@ -41,7 +41,7 @@ $.ajax({
 	url: "https://api.trello.com/1/tokens/7b204829f2a896be8064ed02273e01f32203cf755326c1ebf41974763d812ce4/webhooks/?key=d03c9cf29a52ada649aae8f04b793652",
 	data: {
 		idModel: "58a3c6efd74935bca133a8db",
-		callbackURL: "https://discordapp.com/api/webhooks/366444051274399745/_hlIRmZTkTdvHAdPj7IrMc3D4dQYKSfgno1Z9xO6IqyynHQoDKmIbaeeyYicip5py5sQ"
+		callbackURL: "https://requestb.in/167zk1l1"
 	},
 	success: console.log('Webhook Sent'),
 });
