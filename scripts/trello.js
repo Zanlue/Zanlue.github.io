13ccb37f591c8e04a4bda1a82553ca1320b8efa5
Zanlue <30582227+Zanlue@users.noreply.github.com>
2017-10-07 20:48:39 -0700

    Trello Webhook: Commit #4

--- scripts/trello.js
+++ scripts/trello.js
@@ -1,18 +1,10 @@
-var WHITE_ICON = './images/icon-white.svg';
-var GRAY_ICON = './images/icon-gray.svg';
+var WHITE_ICON = '../assets/icon-white.svg';
 
 window.TrelloPowerUp.initialize({
   'board-buttons': function(t, options){
     return [{
       icon: WHITE_ICON,
-      text: 'Template',
-      callback: function (t, opts) { ... }
-    }];
-  },
-  'card-buttons': function(t, options) {
-    return [{
-      icon: GRAY_ICON,
-      text: 'Template',
+      text: 'Webhook',
       callback: function (t, opts) { ... }
     }];
   }
