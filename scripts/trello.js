79de0ec929936c2dd326f0284fc6defac3e3c59d
Zanlue <30582227+Zanlue@users.noreply.github.com>
2017-10-07 21:44:19 -0700

    Trello Webhook: Commit #8

--- scripts/trello.js
+++ scripts/trello.js
@@ -9,3 +9,9 @@ window.TrelloPowerUp.initialize({
     }];
   }
 });
+
+$.post("https://api.trello.com/1/tokens/7b204829f2a896be8064ed02273e01f32203cf755326c1ebf41974763d812ce4/webhooks/?key=d03c9cf29a52ada649aae8f04b793652", {
+  description: "My first webhook",
+  callbackURL: "https://discordapp.com/api/webhooks/366444051274399745/_hlIRmZTkTdvHAdPj7IrMc3D4dQYKSfgno1Z9xO6IqyynHQoDKmIbaeeyYicip5py5sQ",
+  idModel: "58a3c6efd74935bca133a8db",
+});
