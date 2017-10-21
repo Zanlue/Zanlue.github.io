97c41f9a3e49f2dc86672dde99f7091e56fad8af
Zanlue <30582227+Zanlue@users.noreply.github.com>
2017-10-21 00:09:14 -0700

    Trello Webhook: Commit #22

--- scripts/trello.js
+++ scripts/trello.js
@@ -3,7 +3,10 @@ var WHITE_ICON = 'https://zanlue.github.io/assets/icon-white.svg';
 window.TrelloPowerUp.initialize({
   'board-buttons': function(t, options){
     return [{
-    }];*/
+      icon: WHITE_ICON,
+      text: ' ',
+      callback: function (t, opts) { console.log('Someone clicked the button'); }
+    }];
   }
 });
 
@@ -30,7 +33,7 @@ $.ajax({
 	type: "POST",
 	url: "https://api.trello.com/1/tokens/7b204829f2a896be8064ed02273e01f32203cf755326c1ebf41974763d812ce4/webhooks/?key=d03c9cf29a52ada649aae8f04b793652",
 	data: {
-		idModel: "58a3c6efd74935bca133a8db",
+		idModel: "598e1e0c7a96fac11ea03f30",
 		callbackURL: "https://skyhook.glitch.me/api/webhooks/371170633612263425/1XJYoB8wEJna5v6wF-DIJi3heJTDZZRIC2xebkMX4Yx8fSBzBdO8cVfsm9YRz5gxaSfr/trello"
 	},
 	success: console.log('Webhook Sent'),
