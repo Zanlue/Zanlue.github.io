1bc3c4969911a269f945e9f6f301b053de43e6de
Zanlue <30582227+Zanlue@users.noreply.github.com>
2017-10-20 23:45:17 -0700

    Trello Webhook: Commit #20

--- scripts/trello.js
+++ scripts/trello.js
@@ -1,13 +1,13 @@
 var WHITE_ICON = 'https://zanlue.github.io/assets/icon-white.svg';
 
 window.TrelloPowerUp.initialize({
-  'board-buttons': function(t, options){
+  /*'board-buttons': function(t, options){
     return [{
       icon: WHITE_ICON,
       text: 'Webhook',
       callback: function (t, opts) { console.log('Someone clicked the button'); }
     }];
-  }
+  }*/
 });
 
 var authenticationSuccess = function() {
@@ -29,18 +29,11 @@ window.Trello.authorize({
   error: authenticationFailure
 });
 
-/*window.Trello.post("https://api.trello.com/1/tokens/7b204829f2a896be8064ed02273e01f32203cf755326c1ebf41974763d812ce4/webhooks/?key=d03c9cf29a52ada649aae8f04b793652", {
-  description: "My first webhook",
-  callbackURL: "https://discordapp.com/api/webhooks/366444051274399745/_hlIRmZTkTdvHAdPj7IrMc3D4dQYKSfgno1Z9xO6IqyynHQoDKmIbaeeyYicip5py5sQ",
-  idModel: "58a3c6efd74935bca133a8db",
-  active: true,
-});*/
-
 $.ajax({
 	type: "POST",
 	url: "https://api.trello.com/1/tokens/7b204829f2a896be8064ed02273e01f32203cf755326c1ebf41974763d812ce4/webhooks/?key=d03c9cf29a52ada649aae8f04b793652",
 	data: {
-		idModel: "58a3c6efd74935bca133a8db",
+		idModel: "598e1e0c7a96fac11ea03f30",
 		callbackURL: "https://skyhook.glitch.me/api/webhooks/371170633612263425/1XJYoB8wEJna5v6wF-DIJi3heJTDZZRIC2xebkMX4Yx8fSBzBdO8cVfsm9YRz5gxaSfr/trello"
 	},
 	success: console.log('Webhook Sent'),
