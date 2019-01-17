c5b7851866f77b7289c706f70b6dd65697639c34
zlrc <30582227+zlrc@users.noreply.github.com>
2019-01-16 21:41:23 -0800

    Update index page - Moved media links to the footer - Removed a lot of dead code that was commented out - Adjusted stylesheets.js script to accomodate legacy browsers

--- scripts/stylesheets.js
+++ scripts/stylesheets.js
@@ -1,11 +1,11 @@
 function adjustStyle(width) {
   width = parseInt(width);
-  if (width < 701) {
+  if (width < 701 || !CSS.supports("grid-area: auto")) {
     $("#size-stylesheet").attr("href", "./mobile.css");
   }
   else {
-     $("#size-stylesheet").attr("href", "./main.css"); 
-	 $(document.getElementsByClassName("menu-container")).show();
+    $("#size-stylesheet").attr("href", "./main.css");
+	  $(document.getElementsByClassName("menu-container")).show();
 	  $(document.getElementsByClassName("projectinfo-img")).show();
   }
 }
